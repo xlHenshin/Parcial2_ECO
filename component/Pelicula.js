158928209860f6c8f709acbb814ef6d5245ee7c0
xlHenshin <andreslayer117@gmail.com>
2021-06-05 22:17:22 -0500

    promedio

--- component/Pelicula.js
+++ component/Pelicula.js
@@ -1,11 +1,15 @@
 class Pelicula{
     constructor(pelicula){
         this.pelicula=pelicula
+        this.valorVoto;
     }
 
     render=()=>{
         let component = document.createElement("div");
         component.className = "Pelicula";
+
+        let enviarBtn = document.createElement("button");
+        enviarBtn.className="enviarBtn";
 
         let nombrePeli = document.createElement("div");
         nombrePeli.className="nombrePeli";
@@ -15,69 +19,36 @@ class Pelicula{
         calificacion.className="calificacion";
         calificacion.innerHTML=this.pelicula.calificacion;
 
-        let starOne = document.createElement("input");
-        starOne.type="radio";
-        starOne.id= 'star1';
-        starOne.name='rate';
-        starOne.value="1";
-        let oneLabel = document.createElement("label");
-        oneLabel.htmlFor = 'star1';
-        oneLabel.title= 'text';
-
-
-        let starTwo = document.createElement("input");
-        starTwo.type="radio";
-        starTwo.id= 'star2';
-        starTwo.name='rate';
-        starTwo.value="2";
-        let twoLabel = document.createElement("label");
-        twoLabel.htmlFor = 'star2';
-        twoLabel.title= 'text';
-
-        let starThree = document.createElement("input");
-        starThree.type="radio";
-        starThree.id= 'star3';
-        starThree.name='rate';
-        starThree.value="3";
-        let threeLabel = document.createElement("label");
-        threeLabel.htmlFor = 'star3';
-        threeLabel.title= 'text';
-
-        let starFour = document.createElement("input");
-        starFour.type="radio";
-        starFour.id= 'star4';
-        starFour.name='rate';
-        starFour.value="4";
-        let fourLabel = document.createElement("label");
-        fourLabel.htmlFor = 'star4';
-        fourLabel.title= 'text';
-
-        let starFive = document.createElement("input");
-        starFive.type="radio";
-        starFive.id= 'star5';
-        starFive.name='rate';
-        starFive.value="5";
-        let fiveLabel = document.createElement("label");
-        fiveLabel.htmlFor = 'star5';
-        fiveLabel.title= 'text';
 
         let starContainer = document.createElement("div");
-        starContainer.className="starContainer"
-        starContainer.appendChild(starOne);
-        starContainer.appendChild(oneLabel);
-        starContainer.appendChild(starTwo);
-        starContainer.appendChild(twoLabel);
-        starContainer.appendChild(starThree);
-        starContainer.appendChild(threeLabel);
-        starContainer.appendChild(starFour);
-        starContainer.appendChild(fourLabel);
-        starContainer.appendChild(starFive);
-        starContainer.appendChild(fiveLabel);
+        starContainer.className="rate"
 
-        component.appendChild(nombrePeli);
-        component.appendChild(calificacion);
+        for (let i = 5; i > 0; i--) {
+            let starRate = document.createElement("input");
+            starRate.type='radio';
+            starRate.id= this.pelicula.id+"star"+i;
+            starRate.name="rate"+this.pelicula.id;
+            starRate.value=i;
+            let starLabel = document.createElement("label");
+            starLabel.htmlFor = this.pelicula.id+"star"+i;
+            starLabel.title="text";
+
+            starContainer.appendChild(starRate);
+            starContainer.appendChild(starLabel);
+
+            starRate.addEventListener('click', ()=>{
+                this.valorVoto=starRate.value;
+                //console.log(this.valorVoto);
+            });
+        }
+
+        let infoDiv = document.createElement("div");
+        infoDiv.className="infoDiv";
+
+        infoDiv.appendChild(nombrePeli);
+        infoDiv.appendChild(calificacion);
+        component.appendChild(infoDiv);
         component.appendChild(starContainer);
-
 
         return component;
     }
--- js/index.js
+++ js/index.js
@@ -4,6 +4,10 @@ const username = document.getElementById('username');
 const correo = document.getElementById('correo');
 const logoutBtn = document.getElementById('logoutBtn');
 const peliculaContainer = document.getElementById('peliculaContainer');
+const calificarBtn = document.getElementById('calificarBtn');
+
+let valor1, valor2, valor3, valor4, valor5;
+let arrayComponente = [];
 
 auth.onAuthStateChanged(
     (user)=>{
@@ -15,8 +19,8 @@ auth.onAuthStateChanged(
                 'value',
                 (data)=>{
                     let userDB = data.val();
-                    username.innerHTML = userDB.username;
-                    correo.innerHTML = userDB.correo;
+                    username.innerHTML ="Username: " + userDB.username;
+                    correo.innerHTML ="Correo: " + userDB.correo;
                 }
             );
         }else{
@@ -43,5 +47,43 @@ database.ref('parcial2/peliculas').on('value', function(data){
         let valor = id.val();
         let pelicula = new Pelicula(valor);
         peliculaContainer.appendChild(pelicula.render());
+        arrayComponente.push(pelicula);
     })
+});
+
+calificarBtn.addEventListener('click', ()=>{
+    for (let i = 0; i < arrayComponente.length; i++) {
+        console.log(">>>"+arrayComponente[i].pelicula.nombre);
+        database.ref('parcial2/peliculas/'+arrayComponente[i].pelicula.id).once(
+            'value',
+            (data)=>{
+            let pelisDB=data.val();
+
+            let rateUsuario=parseInt(arrayComponente[i].valorVoto,10);
+            let rateActual=parseInt(pelisDB.calificacion,10);
+            let votosActuales=parseInt(pelisDB.votos,10);
+
+            let promedio= (rateUsuario+(votosActuales*rateActual))/(rateActual+1);
+
+            console.log("Promedio: "+promedio);
+
+            let peliPromedio = {
+                calificacion: promedio,
+                votos: votosActuales+1
+            };
+            database.ref('parcial2/peliculas/'+pelisDB.id).set(peliPromedio);
+        });
+    }
+
+    database.ref('parcial2/users/'+user.uid).once(
+        'value',
+        (data)=>{
+            let userDB = data.val();
+
+            let userVoto={
+                voto: 1
+            };
+            database.ref('parcial2/users/'+userDB.id).set(userVoto);
+        }
+    );
 });
